826ba4b6b1b73e61da202b662a94d4bc38ae7ecc
Andrey Ustyantsev <69751912+vydramain@users.noreply.github.com>
2025-07-01 17:37:05 +0600

    Added searcher

--- init.lua
+++ init.lua
@@ -24,11 +24,10 @@ vim.call('plug#begin')
  Plug('numkil/ag.nvim')                                              	-- A code searching tool similar to ack, with a focus on speed.
  Plug('stevearc/overseer.nvim')                                         -- A task runner and job management plugin for Neovim
  Plug('nvim-lua/plenary.nvim')                                          -- All the lua functions I don't want to write twice
+ Plug('nvim-pack/nvim-spectre')                                         -- Visual interface for project-wide find & replace
 
  Plug('neoclide/coc.nvim')		     	                     	        -- A fast code completion engine
  Plug('deoplete-plugins/deoplete-clang')                                -- To provide C++/C code autocompletion
-
- Plug('Civitasv/cmake-tools.nvim')                                      -- CMake Tools for Neovim
 
  Plug('mfussenegger/nvim-dap')                                        	-- Debug Adapter Protocol client implementation
  Plug('nvim-neotest/nvim-nio')                                          -- A library for asynchronous IO in Neovim, inspired by the asyncio library in Python. The library focuses on providing both common asynchronous primitives and asynchronous APIs for Neovim's core.
@@ -39,7 +38,6 @@ vim.call('plug#begin')
 
  Plug('rhysd/vim-clang-format')                                         -- A (Neo)vim plugin for formatting code.
 
-
 vim.call('plug#end')
 
 -- Vim options
@@ -48,7 +46,7 @@ vim.o.ignorecase = true 						                        -- Case insensitive search
 vim.o.mouse = "v" 							                            -- Middle-click paste with mouse
 vim.o.hlsearch = true 							                        -- Highlight search
 vim.o.incsearch = true 							                        -- Incremental search
-vim.o.tabstop = 4 							                            -- Number of columns occupied by a tab vim.o.softtabstop = 4
+vim.o.tabstop = 2 							                            -- Number of columns occupied by a tab vim.o.softtabstop = 4
 vim.o.expandtab = true 							                        -- Convert tabs to white space
 vim.o.shiftwidth = 4 							                        -- Width for auto-indents
 vim.o.autoindent = true 						                        -- Indent a new line the same amount as the line just typed
@@ -133,6 +131,19 @@ vim.api.nvim_set_keymap('n', '<leader>nf', ':NERDTreeFind<CR>', { noremap = true
 vim.api.nvim_set_keymap('n', '<leader>sh', ':set splitbelow<CR>:split<CR>:buffer %<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sv', ':set splitright<CR>:vsplit<CR>:buffer %<CR>', { noremap = true, silent = true })
 
+-- nvim-spectre keybindings
+vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+    desc = "Toggle Spectre"
+})
+vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+    desc = "Search current word"
+})
+vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+    desc = "Search current word"
+})
+vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+    desc = "Search on current file"
+})
 
 -- Blame line keybindings
 vim.api.nvim_set_keymap('n', '<leader>eb', ':ToggleBlameLine<CR>', { noremap = true, silent = true })
@@ -327,148 +338,6 @@ keyset("n", "<space>k", ":<C-u>CocPrev<cr>", opts)
 -- Resume latest coc list
 keyset("n", "<space>p", ":<C-u>CocListResume<cr>", opts)
 
--- Configuration from README from CMake Tools
-local osys = require("cmake-tools.osys")
-require("cmake-tools").setup {
-  cmake_command = "cmake", -- this is used to specify cmake command path
-  ctest_command = "ctest", -- this is used to specify ctest command path
-  cmake_use_preset = true,
-  cmake_regenerate_on_save = true, -- auto generate when save CMakeLists.txt
-  cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- this will be passed when invoke `CMakeGenerate`
-  cmake_build_options = {}, -- this will be passed when invoke `CMakeBuild`
-  -- support macro expansion:
-  --       ${kit}
-  --       ${kitGenerator}
-  --       ${variant:xx}
-  cmake_build_directory = function()
-    if osys.iswin32 then
-      return "out\\${variant:buildType}"
-    end
-    return "out/${variant:buildType}"
-  end, -- this is used to specify generate directory for cmake, allows macro expansion, can be a string or a function returning the string, relative to cwd.
-  cmake_soft_link_compile_commands = true, -- this will automatically make a soft link from compile commands file to project root dir
-  cmake_compile_commands_from_lsp = false, -- this will automatically set compile commands file location using lsp, to use it, please set `cmake_soft_link_compile_commands` to false
-  cmake_kits_path = nil, -- this is used to specify global cmake kits path, see CMakeKits for detailed usage
-  cmake_variants_message = {
-    short = { show = true }, -- whether to show short message
-    long = { show = true, max_length = 40 }, -- whether to show long message
-  },
-  cmake_dap_configuration = { -- debug settings for cmake
-    name = "cpp",
-    type = "codelldb",
-    request = "launch",
-    stopOnEntry = false,
-    runInTerminal = true,
-    console = "integratedTerminal",
-  },
-  cmake_executor = { -- executor to use
-    name = "quickfix", -- name of the executor
-    opts = {}, -- the options the executor will get, possible values depend on the executor type. See `default_opts` for possible values.
-    default_opts = { -- a list of default and possible values for executors
-      quickfix = {
-        show = "always", -- "always", "only_on_error"
-        position = "belowright", -- "vertical", "horizontal", "leftabove", "aboveleft", "rightbelow", "belowright", "topleft", "botright", use `:h vertical` for example to see help on them
-        size = 10,
-        encoding = "utf-8", -- if encoding is not "utf-8", it will be converted to "utf-8" using `vim.fn.iconv`
-        auto_close_when_success = true, -- typically, you can use it with the "always" option; it will auto-close the quickfix buffer if the execution is successful.
-      },
-      toggleterm = {
-        direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float'
-        close_on_exit = false, -- whether close the terminal when exit
-        auto_scroll = true, -- whether auto scroll to the bottom
-        singleton = true, -- single instance, autocloses the opened one, if present
-      },
-      overseer = {
-        new_task_opts = {
-            strategy = {
-                "toggleterm",
-                direction = "horizontal",
-                auto_scroll = true,
-                quit_on_exit = "success"
-            }
-        }, -- options to pass into the `overseer.new_task` command
-        on_new_task = function(task)
-            require("overseer").open(
-                { enter = false, direction = "right" }
-            )
-        end,   -- a function that gets overseer.Task when it is created, before calling `task:start`
-      },
-      terminal = {
-        name = "Main Terminal",
-        prefix_name = "[CMakeTools]: ", -- This must be included and must be unique, otherwise the terminals will not work. Do not use a simple spacebar " ", or any generic name
-        split_direction = "horizontal", -- "horizontal", "vertical"
-        split_size = 11,
-
-        -- Window handling
-        single_terminal_per_instance = true, -- Single viewport, multiple windows
-        single_terminal_per_tab = true, -- Single viewport per tab
-        keep_terminal_static_location = true, -- Static location of the viewport if avialable
-        auto_resize = true, -- Resize the terminal if it already exists
-
-        -- Running Tasks
-        start_insert = false, -- If you want to enter terminal with :startinsert upon using :CMakeRun
-        focus = false, -- Focus on terminal when cmake task is launched.
-        do_not_add_newline = false, -- Do not hit enter on the command inserted when using :CMakeRun, allowing a chance to review or modify the command before hitting enter.
-      }, -- terminal executor uses the values in cmake_terminal
-    },
-  },
-  cmake_runner = { -- runner to use
-    name = "terminal", -- name of the runner
-    opts = {}, -- the options the runner will get, possible values depend on the runner type. See `default_opts` for possible values.
-    default_opts = { -- a list of default and possible values for runners
-      quickfix = {
-        show = "always", -- "always", "only_on_error"
-        position = "belowright", -- "bottom", "top"
-        size = 10,
-        encoding = "utf-8",
-        auto_close_when_success = true, -- typically, you can use it with the "always" option; it will auto-close the quickfix buffer if the execution is successful.
-      },
-      toggleterm = {
-        direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float'
-        close_on_exit = false, -- whether close the terminal when exit
-        auto_scroll = true, -- whether auto scroll to the bottom
-        singleton = true, -- single instance, autocloses the opened one, if present
-      },
-      overseer = {
-        new_task_opts = {
-            strategy = {
-                "toggleterm",
-                direction = "horizontal",
-                autos_croll = true,
-                quit_on_exit = "success"
-            }
-        }, -- options to pass into the `overseer.new_task` command
-        on_new_task = function(task)
-        end,   -- a function that gets overseer.Task when it is created, before calling `task:start`
-      },
-      terminal = {
-        name = "Main Terminal",
-        prefix_name = "[CMakeTools]: ", -- This must be included and must be unique, otherwise the terminals will not work. Do not use a simple spacebar " ", or any generic name
-        split_direction = "horizontal", -- "horizontal", "vertical"
-        split_size = 11,
-
-        -- Window handling
-        single_terminal_per_instance = true, -- Single viewport, multiple windows
-        single_terminal_per_tab = true, -- Single viewport per tab
-        keep_terminal_static_location = true, -- Static location of the viewport if avialable
-        auto_resize = true, -- Resize the terminal if it already exists
-
-        -- Running Tasks
-        start_insert = false, -- If you want to enter terminal with :startinsert upon using :CMakeRun
-        focus = false, -- Focus on terminal when cmake task is launched.
-        do_not_add_newline = false, -- Do not hit enter on the command inserted when using :CMakeRun, allowing a chance to review or modify the command before hitting enter.
-      },
-    },
-  },
-  cmake_notifications = {
-    runner = { enabled = true },
-    executor = { enabled = true },
-    spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }, -- icons used for progress display
-    refresh_rate_ms = 100, -- how often to iterate icons
-  },
-  cmake_virtual_text_support = true, -- Show the target related to current file using virtual text (at right corner)
-}
-
 -- nvim-dap configuration
 local dap = require('dap')
 local dapui = require('dapui')
@@ -504,4 +373,3 @@ vim.cmd([[
   nmap <Leader>dr <Cmd>lua require'dap'.repl.open()<CR>
   nmap <Leader>dl <Cmd>lua require'dap'.run_last()<CR>
 ]])
-
