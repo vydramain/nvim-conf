9db3303e427580f070fda48e9251e165a9fcfc9e
Andrey Vydra <69751912+vydramain@users.noreply.github.com>
2025-07-10 03:28:38 +0600

    Tried to update some cases

--- lua/options.lua
+++ lua/options.lua
@@ -13,3 +13,10 @@ vim.opt.wrap = false
 vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
 
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
