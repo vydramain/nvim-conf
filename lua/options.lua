98630d45d710c9b152f5e6f0395561a132bc48b3
Andrey Ustyantsev <andrey.ustyantsev@gbooking.ru>
2026-04-14 17:23:57 +0600

    feat: Added title to terminal tab

--- lua/options.lua
+++ lua/options.lua
@@ -45,3 +45,23 @@ opt.completeopt = "menu,menuone,noselect"
 -- Disable netrw (we use neo-tree instead)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+-- Terminal tab title: "nvim | <repo-name>"
+opt.title = true
+
+local function update_title()
+  local git_root = vim.fn.systemlist("git rev-parse --show-toplevel 2>/dev/null")[1]
+  local dir
+  if vim.v.shell_error == 0 and git_root and git_root ~= "" then
+    dir = vim.fn.fnamemodify(git_root, ":t")
+  else
+    dir = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+  end
+  vim.opt.titlestring = "nvim | " .. dir
+end
+
+update_title()
+
+vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged", "BufEnter" }, {
+  callback = update_title,
+})
